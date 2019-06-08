b8d3f9e37a072c4172f8df7580146fe14ca9a922
SuichiM <martiniroqueesteban@gmail.com>
2019-06-08 00:15:01 -0300

    directiva if, y methods para ver/ocultar

--- js/index.js
+++ js/index.js
@@ -2,9 +2,15 @@ const app = new Vue({
     el: '#app',
     template: '#ejemplo',
     data: {
+        mostrar: true,
         mensaje: 'Hola Vue.js',
         imagen: "http://laravelacademy.org/wp-content/uploads/2016/08/00-featured-vuejs-logo-simple-256x128.jpg",
         altimg: "Vue.JS"
 
+    },
+    methods: {
+        toggleMostrar: function () {
+            this.mostrar = !this.mostrar
+        }
     }
 })
