22b43b363270a726947b362942a26a3cb3bb310d
estebanDarriens <esteban@darriens.com>
2019-06-08 00:01:18 -0300

    hello world from Vue.JS

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#app',
+    template: '#ejemplo',
+    data: {
+        mensaje: 'Hola Vue.js',
+        imagen: "http://laravelacademy.org/wp-content/uploads/2016/08/00-featured-vuejs-logo-simple-256x128.jpg",
+        altimg: "Vue.JS"
+
+    }
+})
