2f2c8478f19f09d25834b45edbc4343abbe046af
SuichiM <martiniroqueesteban@gmail.com>
2019-06-08 00:26:14 -0300

    directiva for :D

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const app = new Vue({
     el: '#app',
     template: '#ejemplo',
     data: {
+        cursos: [
+            { name: "Google.com", url: "http://www.google.com" },
+            { name: "Wikipedia.com", url: "http://www.wikipedia.com" }
+        ],
         mostrar: true,
         mensaje: 'Hola Vue.js',
         imagen: "http://laravelacademy.org/wp-content/uploads/2016/08/00-featured-vuejs-logo-simple-256x128.jpg",
